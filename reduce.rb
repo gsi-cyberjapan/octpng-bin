75a180bd8ad8f2f6bc5486c4082933f395c4ff12
Hidenori FUJIMURA <hfu@si.local>
2015-08-11 04:39:01 +0900

    Fix a bug of reduce.rb

--- reduce.rb
+++ reduce.rb
@@ -1,10 +1,11 @@
 require 'chunky_png'
 require 'fileutils'
 
-def write(image, z, x, y)
-  path = "#{z}/#{x}/#{y}.png"
+def write(image, zxy)
+  path = "#{zxy.join('/')}.png"
   FileUtils.mkdir_p(File.dirname(path)) unless File.directory?(File.dirname(path))
   image.save(path)
+  print "wrote #{path}\n"
   return ChunkyPNG::Image.new(256, 256)
 end
 
@@ -18,10 +19,9 @@ while gets
   if current == last or last[0].nil?
     image[u, v] = ChunkyPNG::Color::rgb(255, 0, 0)
   else
-    print "write #{z}/#{x}/#{y}.\n"
-    image = write(image, z, x, y)
+    image = write(image, last)
     image[u, v] = ChunkyPNG::Color::rgb(255, 0, 0)
   end
   last = current
 end
-write(image, z, x, y)
+write(image, last)
