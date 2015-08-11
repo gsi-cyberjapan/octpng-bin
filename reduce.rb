7b39bed15299dcba9b2f0952dd8d959866083018
Hidenori FUJIMURA <hfu@si.local>
2015-08-11 19:47:31 +0900

    Update

--- reduce.rb
+++ reduce.rb
@@ -1,26 +1,32 @@
 require 'chunky_png'
 require 'fileutils'
 
+COLOR_TRUE = ChunkyPNG::Color::rgba(0, 255, 255, 64)
+COLOR_FALSE = ChunkyPNG::Color::rgba(0, 0, 0, 128)
+def _new_image
+  return ChunkyPNG::Image.new(256, 256, COLOR_FALSE)
+end
+
 def write(image, zxy)
   path = "#{zxy.join('/')}.png"
   FileUtils.mkdir_p(File.dirname(path)) unless File.directory?(File.dirname(path))
   image.save(path)
   print "wrote #{path}\n"
-  return ChunkyPNG::Image.new(256, 256)
+  return _new_image
 end
 
 current = [nil, nil, nil]
 last = [nil, nil, nil]
 (z, x, y, u, v) = [nil, nil, nil, nil, nil]
-image = ChunkyPNG::Image.new(256, 256)
+image = _new_image
 while gets
   (z, x, y, u, v) = $_.strip.split(',').map{|v| v.to_i}
   current = [z, x, y]
   if current == last or last[0].nil?
-    image[u, v] = ChunkyPNG::Color::rgb(255, 0, 0)
+    image[u, v] = COLOR_TRUE
   else
     image = write(image, last)
-    image[u, v] = ChunkyPNG::Color::rgb(255, 0, 0)
+    image[u, v] = COLOR_TRUE
   end
   last = current
 end
